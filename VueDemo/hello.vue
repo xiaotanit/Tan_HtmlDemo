d0252bb8b6360c3acbc2f0f83e4cfebc9e82b8b4
xiaotanit@sina.cn <xiaotanit@sina.cn>
2020-01-01 21:46:08 +0800

    update code

---
+++ VueDemo/hello.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>风风火火起步</h1>
+        <h2>跌跌撞撞前进</h2>
+        <h3>逢山开路，遇水搭桥</h3>
+        <h4>百尺竿头，更进一步</h4>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "hello.vue"
+    }
+</script>
+
+<style scoped lang="less">
+
+</style>
